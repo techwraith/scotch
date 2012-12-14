8b33a9812d07adeba3c09b9aab2c007ec77412ad
Techwraith <derickson@yammer-inc.com>
2012-12-13 16:42:31 -0800

    spec out the dashboard controller

--- app/controllers/dashboards.js
+++ app/controllers/dashboards.js
@@ -3,6 +3,9 @@ var Dashboards = function () {
   // this.respondsWith = ['json'];
 
   this.main = function (req, resp, params) {
+    // get all posts that are drafts
+    // get all posts that are published
+    // respond
     this.respond({params: params});
   };
 
@@ -11,13 +14,21 @@ var Dashboards = function () {
   };
 
   this.finish = function (req, resp, param) {
+    // save params for site
+    // set geddy.installed to true
     geddy.installed = true;
+    // TODO: implement a site check in init.js
     this.session.set('user', true);
     this.redirect('/dashboard');
   };
 
   this.analytics = function (req, resp, params) {
+    // get all posts that have been published
+    // get visits for each of them
+    // get unique visitors
+    // respond
     this.respond({params: params});
+    // TODO: set up realtime counts for visits and unique visitors
   };
 
   this.login = function (req, resp, params) {
@@ -25,6 +36,11 @@ var Dashboards = function () {
   };
 
   this.authenticate = function (req, resp, params) {
+    // get site
+    // if password and email === site info
+    //   set this.session.set('user', site);
+    // else
+    //   redirect to login
     this.session.set('user', true);
     this.redirect('/dashboard');
   };
