4bbf730cbf511948da5c1a4054d564baa4023214
Techwraith <techwraithpdx@gmail.com>
2013-06-10 05:54:10 +0000

    even better sorting

--- app/controllers/dashboards.js
+++ app/controllers/dashboards.js
@@ -4,7 +4,7 @@ var Dashboards = function () {
 
   this.main = function (req, resp, params) {
     var self = this;
-    geddy.model.Post.all({isPublished: false}, {sort: {updatedAt: 'ASC'}}, function (err, drafts){
+    geddy.model.Post.all({isPublished: false}, {sort: {updatedAt: 'ASC', createdAt: 'ASC'}}, function (err, drafts){
     geddy.model.Post.all({isPublished: true}, {sort: {createdAt: 'ASC'}}, function (err, posts) {
       self.respond({params: params, posts: posts, drafts: drafts});
     });
