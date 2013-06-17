b34ea1b153e290508d0a0cfecfa2a97beda6c6e6
Daniel Erickson <techwraithpdx@gmail.com>
2013-06-16 23:07:06 -0700

    static site generation

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,10 @@
 var program = require('commander')
   , utils = require('utilities')
   , path = require('path')
-  , geddy = require('geddy');
+  , geddy = require('geddy')
+  , _ = require('underscore')
+  , request = require('request')
+  , fs = require('fs');
 
 program
   .option('create <name>', 'create a new directory (<name>) and generate a new Scotch site in it.')
@@ -25,14 +28,35 @@ var Controller = function () {
     console.log('Serving on port 80');
     console.log(process.cwd())
     geddy.start({
-      environment: 'development'
-    , port: 80
+      environment: 'production'
+    , port: port
     , 'geddy-root': process.cwd()
     });
   };
 
   this.generate = function () {
     console.log('generated static site in ./static');
+    utils.file.cpR(path.join(process.cwd(), 'public'), 'static', {silent: true});
+    geddy.start({'geddy-root': process.cwd(), 'port': 8080});
+    geddy.model.Post.all({isPublished: true}, {sort: {createdAt: 'asc'}}, function (err, posts) {
+      if (err) return console.log(err);
+      var slugs = _.pluck(posts, 'slug')
+        , done = _.after(slugs.length, function () {
+            request('http://localhost:8080/')
+            .on('end', function () {
+              console.log('done');
+              process.exit();
+            })
+            .pipe(fs.createWriteStream(path.join('static', 'index.html')))
+          });
+      for (var i in slugs) {
+        (function (slug, i) {
+          request('http://localhost:8080/'+slug)
+                  .on('end', done)
+                  .pipe(fs.createWriteStream(path.join('static', slug+'.html')))
+        })(slugs[i], i);
+      }
+    });
   };
 
 }
