09d2be336b46d07dbea0adf92bd38d169aa27d1f
Daniel Erickson <techwraithpdx@gmail.com>
2013-06-17 23:31:11 -0700

    closes #30, human error message when mongo is not running

--- config/init.js
+++ config/init.js
@@ -1,5 +1,9 @@
 // Check if there is a site in the DB and set geddy.installed to true if it is
 geddy.model.Site.first(function(err, site) {
+  if (err && err.message.indexOf('failed to connect') > -1 ) {
+    geddy.log.error('Failed to connect to mongo, please make sure it is running.')
+    process.exit();
+  }
   if (!err && site) {
     geddy.installed = true;
     geddy.site = site;
